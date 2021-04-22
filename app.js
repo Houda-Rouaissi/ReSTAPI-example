d4bacc5a5cf9f4cba9b948c65b58d9b46876e831
Houda Rouaissi <35735199+Houda-Rouaissi@users.noreply.github.com>
2021-04-22 14:10:09 +0100

    routes added

--- app.js
+++ app.js
@@ -4,46 +4,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI', { useNewUrlParser: true, useUnifiedTopology: true });
-
 const port = process.env.PORT || 3000;
+const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter')();
 
-const bookRouter = express.Router();
-
-const Book = require('./models/bookModel')
-
-app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-
-bookRouter.route('/books')
-	.post ((req, res) => {
-		const book = new Book(req.body);
-
-		book.save()
-		return res.status(201).json(book);
-	})
-	.get((req, res) => {
-		// const response = { hello : 'This is my API'};
-		const query = {};
-		if (req.query.title) {
-			query.title = req.query.title;
-		}
-		Book.find(query, (err, books) => {
-			if (err) {
-				return res.send(err);
-			}
-			return res.json(books);
-		})
-	});
-
-bookRouter.route('/books/:bookId')
-	.get((req, res) => {
-		Book.findById(req.params.bookId, (err, book) => {
-			if (err) {
-				return res.send(err);
-			}
-			return res.json(book);
-		})
-	});
 
 app.use('/api', bookRouter)
 
